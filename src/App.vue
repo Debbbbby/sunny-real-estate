ca0275a0d5eaa6c7e63d810aa25752b9598fdf94
Deb Lu <deb.tsaiyi@gmail.com>
2021-02-08 23:12:46 +0800

    加入vue router並簡單設置; 更改favicon; 加入views和Home及PropertyDetail頁面; 加入TopBar component; 暫時放入大頭照

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TopBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+	box-sizing: border-box;
 }
+html {
+	scroll-behavior: smooth;
+}
+body {
+	margin: 0;
+	width: 100%;
+}
+.container {
+	margin: 0 auto;
+	max-width: 1200px;
+}
+.clearfix {
+	clear: both;
+}
+img {
+	display: block;
+	width: 100%;
+	height: auto;
+}
+a {
+  text-decoration: none;
+}
+/* #app {
+} */
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="top-bar">
+        <div id="contact-sunny">
+            <img src="../assets/sunny-profile-pic.png" alt="sunny profile pic">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopBar'
+}
+</script>
+
+<style scoped>
+#top-bar {
+    max-height: 70px;
+    background-color: #ffeb3f;
+}
+#contact-sunny {
+    height: 50px;
+}
+</style>
